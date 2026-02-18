b21f8b00a352e3e64f8ae6ef3685acf9d798dd4c
rlova <lovaremano@gmail.com>
2026-02-18 16:14:37 +0100

    class Attaque et Parade

---
+++ src/cartes/Attaque.java
@@ -0,0 +1,10 @@
+package cartes;
+
+public class Attaque extends Bataille {
+
+	protected Attaque(Type type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/cartes/Parade.java
@@ -0,0 +1,10 @@
+package cartes;
+
+public class Parade extends Bataille {
+
+	public Parade(Type type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
+}
