e4a94a4b912967e19545f0dd0e2dbbf442890e8b
rlova <lovaremano@gmail.com>
2026-02-18 17:40:37 +0100

    fin partie 4 du tp1

--- src/cartes/DebuLimite.java
+++ src/cartes/DebuLimite.java
@@ -2,4 +2,9 @@ package cartes;
 
 public class DebuLimite extends Limite {
 
+	@Override
+	public String toString() {
+		return "Limite 50";
+	}
+
 }
--- src/cartes/JeuDeCartes.java
+++ src/cartes/JeuDeCartes.java
@@ -1,33 +1,33 @@
 package cartes;
 
 public class JeuDeCartes {
-	private Configuration[] typesDeCartes = { 
-			new Configuration(new Borne(25),10),
-			new Configuration(new Borne(50),10), 
-			new Configuration(new Borne(75),10), 
-			new Configuration(new Borne(100),12), 
-			new Configuration(new Borne(200),4),
-			new Configuration(new Parade(Type.FEU), 14),
-			new Configuration(new FinLimite(),6),
-			new Configuration(new Parade(Type.ESSENCE),6),
-			new Configuration(new Parade(Type.CREVAISON),6),
-			new Configuration(new Parade(Type.ACCIDENT),6),
-			new Configuration(new Attaque(Type.FEU),5),
-			new Configuration(new DebuLimite(),4),
-			new Configuration(new Attaque(Type.ESSENCE),3),
-			new Configuration(new Attaque(Type.CREVAISON),3),
-			new Configuration(new Attaque(Type.ACCIDENT),3),
-			new Configuration(new Botte(Type.FEU),1),
-			new Configuration(new Botte(Type.ESSENCE),1),
-			new Configuration(new Botte(Type.CREVAISON),1),
-			new Configuration(new Botte(Type.ACCIDENT),1),
-	};
+	private Configuration[] typesDeCartes = new Configuration[19];
+//	private Configuration[] typesDeCartes = { 
+//			new Configuration(new Borne(25),10),
+//			new Configuration(new Borne(50),10), 
+//			new Configuration(new Borne(75),10), 
+//			new Configuration(new Borne(100),12), 
+//			new Configuration(new Borne(200),4),
+//			new Configuration(new Parade(Type.FEU), 14),
+//			new Configuration(new FinLimite(),6),
+//			new Configuration(new Parade(Type.ESSENCE),6),
+//			new Configuration(new Parade(Type.CREVAISON),6),
+//			new Configuration(new Parade(Type.ACCIDENT),6),
+//			new Configuration(new Attaque(Type.FEU),5),
+//			new Configuration(new DebuLimite(),4),
+//			new Configuration(new Attaque(Type.ESSENCE),3),
+//			new Configuration(new Attaque(Type.CREVAISON),3),
+//			new Configuration(new Attaque(Type.ACCIDENT),3),
+//			new Configuration(new Botte(Type.FEU),1),
+//			new Configuration(new Botte(Type.ESSENCE),1),
+//			new Configuration(new Botte(Type.CREVAISON),1),
+//			new Configuration(new Botte(Type.ACCIDENT),1),
+//	};
 	
 	public String affichageJeuDeCartes() {
 		StringBuilder affichage = new StringBuilder();
-		affichage.append("JEU :\n");
 		for (Configuration conf : typesDeCartes) {
-			affichage.append(conf.getNbExemplaires()).append(' ').append(conf.getCarte());
+			affichage.append(conf.getNbExemplaires()).append(' ').append(conf.getCarte()).append('\n');
 		}
 		return affichage.toString();
 	}
@@ -35,6 +35,45 @@ public class JeuDeCartes {
 	public JeuDeCartes(Configuration[] typesDeCartes) {
 		super();
 		this.typesDeCartes = typesDeCartes;
+	}
+	
+	public JeuDeCartes() {
+		typesDeCartes[0] = new Configuration(new Borne(25),10);
+		typesDeCartes[1] = new Configuration(new Borne(50),10);
+		typesDeCartes[2] = new Configuration(new Borne(75),10);
+		typesDeCartes[3] = new Configuration(new Borne(100),12);
+		typesDeCartes[4] = new Configuration(new Borne(200),4);
+		typesDeCartes[5] = new Configuration(new Parade(Type.FEU), 14);
+		typesDeCartes[6] = new Configuration(new FinLimite(),6);
+		typesDeCartes[7] = new Configuration(new Parade(Type.ESSENCE),6);
+		typesDeCartes[8] = new Configuration(new Parade(Type.CREVAISON),6);
+		typesDeCartes[9] = new Configuration(new Parade(Type.ACCIDENT),6);
+		typesDeCartes[10] = new Configuration(new Attaque(Type.FEU),5);
+		typesDeCartes[11] = new Configuration(new DebuLimite(),4);
+		typesDeCartes[12] = new Configuration(new Attaque(Type.ESSENCE),3);
+		typesDeCartes[13] = new Configuration(new Attaque(Type.CREVAISON),3);
+		typesDeCartes[14] = new Configuration(new Attaque(Type.ACCIDENT),3);
+		typesDeCartes[15] = new Configuration(new Botte(Type.FEU),1);
+		typesDeCartes[16] = new Configuration(new Botte(Type.ESSENCE),1);
+		typesDeCartes[17] = new Configuration(new Botte(Type.CREVAISON),1);
+		typesDeCartes[18] = new Configuration(new Botte(Type.ACCIDENT),1);
+	}
+
+	public Carte[] donnerCartes() {
+		int totalCartes = 0;
+		for (Configuration c : typesDeCartes) {
+			totalCartes += c.getNbExemplaires();
+		}
+		Carte[] cartes = new Carte[totalCartes];
+		int numCarte = 0;
+		for (Configuration c : typesDeCartes) {
+			Carte carte = c.getCarte();
+			for (int i=0; i<c.getNbExemplaires();i++) {
+				cartes[numCarte] = carte;
+				numCarte++;
+			}
+		}
+		return cartes;
 	}
 	
 //	classe interne
